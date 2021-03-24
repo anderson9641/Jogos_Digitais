3bf4d1a826d36fa2f20943cd2d88efac68cd33f7
Anderson Alves <anderson96627210@gmail.com>
2021-03-24 19:24:26 -0300

    Menu Inicial

---
+++ Puzle Color/Assets/Scripts/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public string cena;
+    public GameObject painelOption;
+
+    void Update()
+    {
+        
+    }
+
+    public void startGame(){
+        SceneManager.LoadScene(cena);
+    }
+
+    public void quitGame(){
+        //Editor Unity
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //Jogo compilado
+        //Applicatio.Quit();
+
+    }
+
+    public void showPainel(){
+        painelOption.SetActive(true);
+    }
+
+    public void backToMenu(){
+        painelOption.SetActive(false);
+    }
+}
