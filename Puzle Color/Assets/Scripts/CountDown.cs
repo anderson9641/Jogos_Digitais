1fe3881fa35cb69e04b3ede3c7aff18c78633519
Anderson Alves <anderson96627210@gmail.com>
2021-03-28 11:24:40 -0300

    Fase 1

---
+++ Puzle Color/Assets/Scripts/CountDown.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    public Text DisplayMinutos, DisplaySegundos;
+    public float tempo = 120;
+    private int minutos, segundos;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (tempo > 0)
+        {
+            minutos = (int)(tempo / 60);
+            segundos = (int)(tempo % 60);
+
+            tempo -= Time.deltaTime;
+            DisplayMinutos.text = minutos.ToString("F0");
+            DisplaySegundos.text = segundos.ToString("F0");
+        }
+    }
+}
