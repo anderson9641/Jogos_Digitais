7386189b734fc140d897b5787db8d1cf6ea377f1
Anderson Alves <anderson96627210@gmail.com>
2021-04-14 14:48:52 -0300

    Projeto original

---
+++ PuzzleColor/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    private Vector3 dir;
+    private Rigidbody rb;
+
+    [SerializeField] private float rY;
+    [SerializeField] private float rX;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = player.TransformVector(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized);
+
+        rX = Mathf.Lerp(rX, Input.GetAxis("Mouse X") * 2, 100 * Time.deltaTime);
+        rY = Mathf.Clamp(rY - Input.GetAxis("Mouse Y")* 2 * 100 * Time.deltaTime, -30, 30);
+
+        player.Rotate(0, rX, 0, Space.World);
+
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.MovePosition(rb.position + dir * 10 * Time.fixedDeltaTime);
+        rb.velocity = dir * 10;
+    }
+}
---
+++ PuzzleColor/Assets/Scripts/SystemColor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemColor : MonoBehaviour
+{
+    public Color[] cores;
+    private Rigidbody rg;
+    private string CurrentColor = "Branco";
+    [SerializeField] GameObject sprite;
+ 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("CuboAzul"))
+        {
+            Color cor = cores[1];
+            sprite.GetComponent<Renderer>().material.color = cor;
+            Destroy(other.gameObject);
+            CurrentColor = "Azul";
+
+        }else if (other.tag.Equals("CuboAmarelo"))
+        {
+            Color cor = cores[0];
+            sprite.GetComponent<Renderer>().material.color = cor;
+            other.gameObject.SetActive(false);
+            //Destroy(other.gameObject);
+            CurrentColor = "Amarelo";
+
+        }else if (other.tag.Equals("CuboVermelho"))
+        {
+            Color cor = cores[2];
+            sprite.GetComponent<Renderer>().material.color = cor;
+            Destroy(other.gameObject);
+            CurrentColor = "Vermelha";
+        }
+
+        
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("ParedeAzul") && CurrentColor.Equals("Azul"))
+        {
+            Destroy(other.gameObject);
+
+        }else if (other.collider.CompareTag("ParedeAmarela") && CurrentColor.Equals("Amarelo"))
+        {
+            
+            Destroy(other.gameObject);
+
+        }else if (other.collider.CompareTag("ParedeVermelha") && CurrentColor.Equals("Vermelha"))
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    
+}
