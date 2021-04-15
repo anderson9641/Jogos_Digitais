3d4f9b2b105e013a25273e72b06bbc29f2a7cafe
Anderson Alves <anderson96627210@gmail.com>
2021-04-15 11:10:59 -0300

    animações

---
+++ PuzzleColor/Assets/Scripts/Animação.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animação : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            anim.SetInteger("transicao", -1);
+        }else if(Input.GetKey(KeyCode.S)){
+            anim.SetInteger("transicao", 1);
+        }else if(Input.GetKey(KeyCode.A)){
+            anim.SetInteger("transicao", -2);
+        }else if(Input.GetKey(KeyCode.D)){
+            anim.SetInteger("transicao", 2);
+        }else{
+            anim.SetInteger("transicao", 0);
+        }
+    }
+}
