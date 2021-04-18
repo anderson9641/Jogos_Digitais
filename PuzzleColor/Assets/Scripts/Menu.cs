805d34b93f504ef0f7b449a23c47a2c4e3f5125d
Anderson Alves <anderson96627210@gmail.com>
2021-04-17 23:23:26 -0300

    criação do Menu

---
+++ PuzzleColor/Assets/Scripts/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject painelTutorial;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void starGame(){
+        SceneManager.LoadScene("Level-1");
+    }
+
+    public void quitGame(){
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //Jogo Compilado
+        //Application.Quit();
+    }
+
+    public void showTutorial(){
+        painelTutorial.SetActive(true);
+    }
+    public void backToMenu(){
+        painelTutorial.SetActive(false);
+    }
+}
