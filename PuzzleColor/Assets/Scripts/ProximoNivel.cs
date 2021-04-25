56f364f62424abd2114a55840f50bc76d8d3dc11
Anderson Alves <anderson96627210@gmail.com>
2021-04-24 22:29:29 -0300

    atualização cena 4

---
+++ PuzzleColor/Assets/Scripts/ProximoNivel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProximoNivel : MonoBehaviour
+{   
+    public string proximoLevel;
+    public void OnTriggerEnter(Collider other){
+         if(other.tag.Equals("Player")){
+             SceneManager.LoadScene(proximoLevel);
+         }
+     }
+}
