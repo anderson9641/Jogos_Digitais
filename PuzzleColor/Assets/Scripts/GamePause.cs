66c6b9ffb5465ef6e02f1c1fc5c1f6f6318d5c59
Anderson Alves <anderson96627210@gmail.com>
2021-05-08 19:43:35 -0300

    pontuação

---
+++ PuzzleColor/Assets/Scripts/GamePause.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+    public GameObject telaDePause;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                telaDePause.SetActive(true);
+                Cursor.lockState = CursorLockMode.None;
+
+            }
+            else
+            {
+                exitToGame();
+            }
+        }
+    }
+    public void exitToMenu()
+    {
+        
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void restartLevel()
+    {
+        Scene cena = SceneManager.GetActiveScene();
+        exitToGame();
+        SceneManager.LoadScene(cena.name);
+    }
+
+    public void exitToGame()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        telaDePause.SetActive(false);
+    }
+}
--- PuzzleColor/Assets/Scripts/Temporizador.cs
+++ PuzzleColor/Assets/Scripts/Temporizador.cs
@@ -2,29 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Temporizador : MonoBehaviour
 {
-    
+
     public Text textoTempo;
+    public Text textoScore;
+    private int scoreGanho;
     int minutos;
     int segundos;
     public float tempoTotal;
-    void Update()
+    private float tempoGasto;
+    public string proximoLevel;
+    
+    private void Start()
     {
-        tempoTotal -= Time.deltaTime;
-        minutos = (int) (tempoTotal / 60);
-        segundos = (int) (tempoTotal % 60);
-        if(segundos < 10){
-            textoTempo.text = "0" + minutos.ToString("F0") + " : "+ "0" + segundos.ToString("F0");
-        }else{
-            textoTempo.text = "0" + minutos.ToString("F0") + " : " + segundos.ToString("F0");
+        tempoGasto = tempoTotal;
+        if (proximoLevel == "Level-2")
+        {
+            PlayerPrefs.SetInt("score", 0);
         }
-        if(minutos <= 0 && segundos <= 0){
-            textoTempo.text = " Game Over ";
+        else
+        {            
+            scoreGanho = PlayerPrefs.GetInt("score");
+            textoScore.text = scoreGanho.ToString("F0");
         }
-        
-        
 
     }
+    void Update()
+    {
+
+        tempoGasto -= Time.deltaTime;
+        minutos = (int)(tempoGasto / 60);
+        segundos = (int)(tempoGasto % 60);
+        if (segundos < 10)
+        {
+            textoTempo.text = "0" + minutos.ToString("F0") + " : " + "0" + segundos.ToString("F0");
+        }
+        else
+        {
+            textoTempo.text = "0" + minutos.ToString("F0") + " : " + segundos.ToString("F0");
+        }
+        if (minutos <= 0 && segundos <= 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            scoreGanho = scoreGanho + ((int)tempoGasto);
+            PlayerPrefs.SetInt("score", scoreGanho);
+            textoScore.text = scoreGanho.ToString("F0");
+            SceneManager.LoadScene(proximoLevel);
+        }
+    }
+
 }
